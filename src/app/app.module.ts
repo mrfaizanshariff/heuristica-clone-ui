cfc535d7e784b0d0f2af1b84b52fdcea41405319
Mohammed Faizan Shariff <71890836+mrfaizanshariff@users.noreply.github.com>
2024-03-07 12:23:55 +0530

    ng reactflow library from dotglitch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './components/test-component/react-flow-test.component';
+import { ReactsFlowComponent } from './components/react-flow/react-flow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    TestComponent,
+    AppRoutingModule,
+    ReactsFlowComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
